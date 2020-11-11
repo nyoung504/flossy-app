25440b0bffba7abb8dcd242ef403979d206cffa0
Nicole Young <coley504@gmail.com>
2020-11-12 09:18:22 +1100

    active storage for pictures

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -70,6 +70,6 @@ class ListingsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def listing_params
-      params.require(:listing).permit(:title, :description, :brand, :price, :size, :category, :user_id)
+      params.require(:listing).permit(:title, :description, :brand, :price, :size, :category, :user_id, :image)
     end
-end
+  end
